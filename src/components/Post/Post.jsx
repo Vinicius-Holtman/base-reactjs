6d772a700e82fefa547b579773215bf33723f886
Vinicius <viniholtman28@gmail.com>
2022-06-02 16:37:44 -0300

    Created structure for my component Post

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,28 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img className={styles.avatar} src="https://github.com/Vinicius-Holtman.png" />
+            <div className={styles.authorInfo}>
+              <strong>Vinicius Holtman</strong>
+              <span>Fullstack Developer</span>
+            </div>
+          </div>
+
+          <time title='11 de Maio às 10:13' dateTime='2002-05-11 08:10:13'>Públicado há 1h</time>
+        </header>
+
+        <div className={styles.content}>
+          <p> Fala galeraa 👋</p>
+          <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+          <p> 👉 <a href='#'>jane.design/doctorcare</a></p>
+          <p> <a href='#'>#novoprojeto #nlw #rocketseat</a></p>
+        </div>
+      </article>
+    </>
+  )
+}
