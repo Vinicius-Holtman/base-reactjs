5987ef124c5dd4aa10fd45bb98b06f2b290c9751
Vinicius <viniholtman28@gmail.com>
2022-06-02 13:47:40 -0300

    Created component Header

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,9 @@
+import { Header } from "./Header";
+
 export function Post(props) {
   return (
     <>
+      <Header />
       <strong>{props.author}</strong>
       <p>{props.content}</p>
     </>
