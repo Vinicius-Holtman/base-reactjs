9f73cbf88d46f26ecbbd24b62034e5ead5f214bb
Vinicius <viniholtman28@gmail.com>
2022-06-02 15:56:29 -0300

    Stylized component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Post } from "./components/Post";
+
+import styles from './App.module.css'
 
 export function App() {
   return (
     <>
-     <Post author="Vinicius" content="alguam descrição" />
-     <Post author="Outro autor" content="alguam descrição" />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Vinicius" content="alguam descrição" />
+          <Post author="Outro autor" content="alguam descrição" />
+        </main>
+      </div>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/image-logo.svg'
+
 export function Header() {
   return (
     <>
+      <img src={igniteLogo} alt="Ignite logo" />
       <strong className={styles.header}>Ignite Feed</strong>
     </>
   )
