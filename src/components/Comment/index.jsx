0d0527a6ecb1c7ef6fa053e95546f09e71adb598
Vinicius <viniholtman28@gmail.com>
2022-06-16 22:00:37 -0300

    Create architecture to compoentn comment

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Vinicius-Holtman.png" alt="Imagem Avatar" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vinicius Holtman</strong>
+              <time title='11 de Maio às 10:13' dateTime='2002-05-11 08:10:13'>Públicado há 1h</time>
+            </div>
+
+            <button title='Deletar comentario'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabens!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
