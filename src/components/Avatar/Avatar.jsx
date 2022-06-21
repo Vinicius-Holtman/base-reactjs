625669f1f2e4103e231940a0400f39f48f0a3339
Vinicius <viniholtman28@gmail.com>
2022-06-20 22:17:31 -0300

    Create array for list information of users

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,6 +3,5 @@ import styles from './Avatar.module.css'
 export function Avatar({hasBorder = true, src}) {
   return (
     <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} />
-
   )
 }
