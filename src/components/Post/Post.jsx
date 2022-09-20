64da16e21dd40281e961c7c9bd9129afb4717499
Vinicius <viniholtman28@gmail.com>
2022-09-20 15:03:27 -0300

    Fixed key

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -47,9 +47,9 @@ export function Post({ author, publishedAt, content }) {
         <div className={styles.content}>
           {content.map(line => {
             if(line.type === 'paragraph') {
-              return <p>{line.content}</p>
+              return <p key={line.content}>{line.content}</p>
             } else if (line.type === 'link') {
-              return <p><a href="#">{line.content}</a></p>
+              return <p key={line.content}><a href="#">{line.content}</a></p>
             }
           })}
         </div>
@@ -70,8 +70,8 @@ export function Post({ author, publishedAt, content }) {
         </form>
 
         <div className={styles.commentList}>
-          {comments.map((comment, i) => (
-            <Comment key={i} comment={comment} />
+          {comments.map(comment => (
+            <Comment key={comment} comment={comment} />
           ))}
         </div>
       </article>
